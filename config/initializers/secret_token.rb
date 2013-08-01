eaa7ed926b5e4f466a03291736178e03bc6bd752
Szpetny Frank <szpetny.frank@gmail.com>
2013-08-01 17:27:54 +0200

    modified ignore and secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-BookerApp::Application.config.secret_key_base = '5cc818acc55d7ec1179a71ad18a7ea9a0f46d0aa2e13664d6946d9336c968074efe51ca0e70c1257dd213f808d9c22e2586eee37a1b7cc5f507befe008f2bb33'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+BookerApp::Application.config.secret_key_base = secure_token
