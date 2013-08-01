9ea18c45428ea32a1cd4ea8918708b69226e509d
Szpetny Frank <szpetny.frank@gmail.com>
2013-08-01 12:07:48 +0200

    model added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 BookerApp::Application.routes.draw do
+  resources :books
+
+  resources :authors
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
