44fdd375cc7cff12b8eb729b202c2121a0c0f1fb
Szpetny Frank <szpetny.frank@gmail.com>
2013-08-02 19:46:47 +0200

    tests modified

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,28 +1,41 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-=begin
-describe "GET /static_pages" do
-it "works! (now write some real specs)" do
-# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-get static_pages_index_path
-response.status.should be(200)
-end
-end
-=end
+
   subject { page }
+  
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
   
   describe "Home page" do
     before { visit root_path }
     
-    it { should have_content(I18n.t(:main_app_title)) }
-    it { should have_title(full_title("Home")) }
+    let(:heading)    { full_title("Home") }
+    let(:page_title) { "Home" }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Help page" do
     before { visit help_path }
     
-    it { should have_content('Help') }
-    it { should have_title(full_title("Help")) }
+    let(:heading)    { full_title("Help") }
+    let(:page_title) { "Help" }
+
+    it_should_behave_like "all static pages"
+  end
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Home"
+    expect(page).to have_title(full_title('Home'))
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title('Home'))
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,1 @@
-def full_title(page_title)
-  base_title = I18n.t(:main_app_title)
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
