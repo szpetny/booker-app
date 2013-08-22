d9fd9fddf897631a62ff27bd3a615adcb30ac7b9
Szpetny Frank <szpetny.frank@gmail.com>
2013-08-22 11:07:51 +0200

    datatable PL messages fix

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -2,23 +2,7 @@ $(function() {
   $('#books').dataTable({
         "sDom": "<'row'<'span'l><'span'f>r>t<'row'<'span'i><'span'p>>",
         "sPaginationType": "bootstrap",
-        "oLanguage": {
-		    "sProcessing":   "Proszę czekać...",
-		    "sLengthMenu":   "Pokaż _MENU_ pozycji",
-		    "sZeroRecords":  "Nie znaleziono żadnych pasujących indeksów",
-		    "sInfo":         "Pozycje od _START_ do _END_ z _TOTAL_ łącznie",
-		    "sInfoEmpty":    "Pozycji 0 z 0 dostępnych",
-		    "sInfoFiltered": "(filtrowanie spośród _MAX_ dostępnych pozycji)",
-		    "sInfoPostFix":  "",
-		    "sSearch":       "Szukaj:",
-		    "sUrl":          "",
-		    "oPaginate": {
-		    "sFirst":    "Pierwsza",
-		    "sPrevious": "Poprzednia",
-		    "sNext":     "Następna",
-		    "sLast":     "Ostatnia"
-		    }
-		}
+        "oLanguage": {"sUrl": "dataTables/polish.txt"}
   });
 });
     
